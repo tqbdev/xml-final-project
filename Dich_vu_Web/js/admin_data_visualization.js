9846886fb53672f36d239110dc950ea5f71846d6
tqbdev <tranquocbao3897@gmail.com>
2018-06-11 23:28:45 +0700

    Add more data and fix some bugs

--- Dich_vu_Web/js/admin_data_visualization.js
+++ Dich_vu_Web/js/admin_data_visualization.js
@@ -1,55 +1,5 @@
 "use strict";
 
-// Load google charts
-google.charts.load('current', {
-      'packages': ['corechart']
-});
-google.charts.setOnLoadCallback(drawChart);
-
-// Draw the chart and set the chart values
-function drawChart() {
-      var dataItems = google.visualization.arrayToDataTable([
-            ['Loại', 'Số lượng'],
-            ['Truyện tranh', 80],
-            ['Trinh thám', 20],
-            ['Tiểu thuyết', 20],
-            ['Báo chí', 30],
-            ['Tạp chí', 20],
-            ['Văn học', 70]
-      ]);
-
-      // Optional; add a title and set the width and height of the chart
-      var optionsItems = {
-            'title': 'Số sản phẩm theo loại',
-            'width': 'auto',
-            'height': 400
-      };
-
-      var dataRevenue = google.visualization.arrayToDataTable([
-            ['Doanh thu', 'Số tiền'],
-            ['Truyện tranh', 1000000],
-            ['Trinh thám', 2000000],
-            ['Tiểu thuyết', 3000000],
-            ['Báo chí', 1000000],
-            ['Tạp chí', 2000000],
-            ['Văn học', 3000000]
-      ]);
-
-      // Optional; add a title and set the width and height of the chart
-      var optionsRevenue = {
-            'title': 'Doanh thu theo loại',
-            'width': 'auto',
-            'height': 400
-      };
-
-      // Display the chart inside the <div> element with id="piechart"
-      var chart = new google.visualization.PieChart(document.getElementById('piechartItems'));
-      chart.draw(dataItems, optionsItems);
-
-      var chart = new google.visualization.PieChart(document.getElementById('piechartRevenue'));
-      chart.draw(dataRevenue, optionsRevenue);
-}
-
 const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
       'November', 'December'
 ];
